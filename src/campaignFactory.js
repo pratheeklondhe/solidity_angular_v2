555dc9743d5fcfded399688b99b591da78c9dcd9
Pratheek Londhe <41989079+pratheeklondhe@users.noreply.github.com>
2022-05-08 10:21:59 +0530

    campaign contract web3 setup

---
+++ src/campaignFactory.js
@@ -0,0 +1,9 @@
+import web3 from "./web3";
+import * as CampaignFactory from "./builds/CampaignFactory.json";
+
+const CampaignFactoryInstance = new web3.eth.Contract(
+  JSON.parse(CampaignFactory.abi),
+  "0x68F4D7e49595Aa7B66CdA8ce9b32D8e6FF362207"
+);
+
+export default CampaignFactoryInstance;
