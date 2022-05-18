e41e0995c045253aa24ea2e1ca54d69942246758
Pratheek Londhe <41989079+pratheeklondhe@users.noreply.github.com>
2022-05-18 12:00:08 +0530

    button primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CardModule } from "primeng/card";
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CardModule],
+  imports: [BrowserModule, BrowserAnimationsModule, CardModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
