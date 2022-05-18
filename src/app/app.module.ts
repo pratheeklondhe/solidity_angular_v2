5c8ac00d1501dbc3a31ccbff83243198baf6af4e
Pratheek Londhe <41989079+pratheeklondhe@users.noreply.github.com>
2022-05-18 11:50:03 +0530

    material removed and primeNG added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
 import { AppComponent } from "./app.component";
-import { MatCardModule } from "@angular/material/card";
-import { MatCheckboxModule } from "@angular/material/checkbox";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CardModule } from "primeng/card";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatCardModule,
-    MatCheckboxModule
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, CardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
