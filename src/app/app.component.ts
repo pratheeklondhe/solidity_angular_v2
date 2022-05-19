313cb203e0248e5dc02cc73bdd7402265563fc0a
Pratheek Londhe <41989079+pratheeklondhe@users.noreply.github.com>
2022-05-19 13:57:49 +0530

    home compoennt added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from "@angular/core";
-import factory from "../campaignFactory";
-import web3 from "../web3";
-import { MessageService } from "primeng/api";
-import { Router } from "@angular/router";
 
 @Component({
   selector: "app-root",
@@ -10,39 +6,5 @@ import { Router } from "@angular/router";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "CodeSandbox";
-  campaigns = [];
-  accounts = [];
-
-  constructor(private router: Router, private messageService: MessageService) {}
-
-  async ngOnInit() {
-    this.accounts = await web3.eth.requestAccounts();
-    console.log(this.accounts);
-    this.getDeployedCampaigns();
-  }
-
-  async getDeployedCampaigns() {
-    this.campaigns = await factory.methods.getDeployedCampaigns().call({});
-  }
-
-  async createCampaign() {
-    try {
-      await factory.methods.createCampaign(1).send({
-        from: this.accounts[0],
-        gas: "10000000"
-      });
-
-      this.getDeployedCampaigns();
-    } catch (e) {
-      this.messageService.add({
-        severity: "error",
-        summary: "Failed to create Campaign"
-      });
-    }
-  }
-
-  viewCampaign() {
-    this.router.navigate(["/camp"]);
-  }
+  constructor() {}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { MessageService } from "primeng/api";
+import factory from "../../campaignFactory";
+import web3 from "../../web3";
 
 @Component({
   selector: "app-home",
@@ -6,11 +10,38 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-  constructor() {
-    console.log("Campaign Component");
+  campaigns = [];
+  accounts = [];
+
+  constructor(private router: Router, private messageService: MessageService) {}
+
+  async ngOnInit() {
+    this.accounts = await web3.eth.requestAccounts();
+    console.log(this.accounts);
+    this.getDeployedCampaigns();
   }
 
-  ngOnInit(): void {
-    console.log("Campaign Componentfdsf");
+  async getDeployedCampaigns() {
+    this.campaigns = await factory.methods.getDeployedCampaigns().call({});
+  }
+
+  async createCampaign() {
+    try {
+      await factory.methods.createCampaign(1).send({
+        from: this.accounts[0],
+        gas: "10000000"
+      });
+
+      this.getDeployedCampaigns();
+    } catch (e) {
+      this.messageService.add({
+        severity: "error",
+        summary: "Failed to create Campaign"
+      });
+    }
+  }
+
+  viewCampaign() {
+    this.router.navigate(["/camp"]);
   }
 }
